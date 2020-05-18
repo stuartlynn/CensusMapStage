24d9f854eddd307129a623697cc1e4fee5e5e052
Stuart Lynn <stuart.lynn@gmail.com>
2020-05-18 13:58:21 -0400

    Updates

---
+++ precache-manifest.16967547f1b68b2766a69e2f38a12e2c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f071409c766b7f5a4d8a7755fc48f2",
+    "url": "https://stuartlynn.github.io/CensusMapStage/index.html"
+  },
+  {
+    "revision": "556e0841fe62cfa362df",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/css/2.76609786.chunk.css"
+  },
+  {
+    "revision": "60069a06e6ec1b802f5d",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/css/main.29e5a28c.chunk.css"
+  },
+  {
+    "revision": "556e0841fe62cfa362df",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/js/2.ef50664f.chunk.js"
+  },
+  {
+    "revision": "e928fe3a96a63c8d640bfb97f49535de",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/js/2.ef50664f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60069a06e6ec1b802f5d",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/js/main.be9eaa1e.chunk.js"
+  },
+  {
+    "revision": "aedb8572742e4598bfd2",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/js/runtime-main.f1e3b9f8.js"
+  },
+  {
+    "revision": "1313c346b5eefdd2c3fd440bc9b18002",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/media/GT-America-Standard-Bold.1313c346.otf"
+  },
+  {
+    "revision": "ac9e5f9ba79e0a6c4e0c72a295fde8cf",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/media/GT-America-Standard-Regular.ac9e5f9b.otf"
+  },
+  {
+    "revision": "557633ad25205c6875c6a68e9d7ff0e6",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/media/laundry_484949.557633ad.svg"
+  },
+  {
+    "revision": "59ec3d76819bc4da23eb60a9c3547fd8",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/media/laundry_A44098.59ec3d76.svg"
+  },
+  {
+    "revision": "9d18bb14727e1151f0de7ad17bf7c33d",
+    "url": "https://stuartlynn.github.io/CensusMapStage/static/media/laundry_A44098_alert.9d18bb14.svg"
+  }
+]);
